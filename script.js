696f8e26e1ddca8194fc567639962df2d2143130
BSharma1993 <142150720+BSharma1993@users.noreply.github.com>
2023-11-01 17:13:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-let x = document.getElementById("nav-list");
-let menubtn = document.querySelector(".menubtn");
-let closebtn = document.querySelector(".closebtn");
 
-function openMenu() {
-  x.style.display = "block";
-  menubtn.style.display = "none";
-  closebtn.style.display = "block";
-}
-function closeMenu() {
-  x.style.display = "none";
-  menubtn.style.display = "block";
-  closebtn.style.display = "none";
-}
 
 gsap.from(".main", 1.5, { opacity: 0, y: -300, delay: 0.2 });
 gsap.from(".main2", 1.5, { opacity: 0, y: -300, delay: 0.5 });
